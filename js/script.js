f514f51739d9e5d77dc37feb38ec0a90e3d222fe
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-03-11 14:23:57 +0100

    [add] Reverse and button

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ createApp({
 
   data(){
     return {
-      message: '',
+      message: 'Edit me!!',
       count: 0
     }
   },
@@ -14,6 +14,10 @@ createApp({
   methods: {
     increment(){
       this.count++
+    },
+
+    reverse(){
+      this.message = this.message.split('').reverse().join('')
     }
   }
 
